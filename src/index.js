cff166bf624d23ccecba829e12fd72535cb80c12
dgls-och <241841968+dgls-och@users.noreply.github.com>
2026-05-28 19:15:14 +0100

    Add Task class

--- src/index.js
+++ src/index.js
@@ -40,3 +40,17 @@ const todoList = (function () {
 
   return { getList, addCategory, addTask, removeCategory, removeTask };
 })();
+
+class Task {
+  constructor(title, description, priority, dueDate, done) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.done = done;
+  }
+
+  isDone() {
+    this.done === false ? (this.done = true) : (this.done = false);
+  }
+}
